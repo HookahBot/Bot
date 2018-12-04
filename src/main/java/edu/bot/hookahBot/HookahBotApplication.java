4c7906d454cc3cf95a011b5668a37c7f8f6aab68
wowaolex <vovaolex123@gmail.com>
2018-12-04 16:20:29 +0200

    initial commit

---
+++ src/main/java/edu/bot/hookahBot/HookahBotApplication.java
@@ -0,0 +1,12 @@
+package edu.bot.hookahBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookahBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookahBotApplication.class, args);
+	}
+}
